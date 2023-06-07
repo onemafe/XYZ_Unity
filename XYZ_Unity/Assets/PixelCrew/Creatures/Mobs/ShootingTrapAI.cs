f6ac887f9e99c636a46f1ea9f49089b4aba3e21d
onemafe <onemafe@gmail.com>
2023-06-07 18:17:48 +0300

    Реализовал плюющуюся ракушку

---
+++ XYZ_Unity/Assets/PixelCrew/Creatures/Mobs/ShootingTrapAI.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PixelCrew;
+using PixelCrew.Components;
+using PixelCrew.Utils;
+using UnityEngine;
+
+public class ShootingTrapAI : MonoBehaviour
+{
+    [SerializeField] private LayerCheck _vision;
+
+
+    [Header("Melee")]
+    [SerializeField] private CheckCircleOverlap _meleeAttack;
+    [SerializeField] private LayerCheck _meleeCanAttack;
+    [SerializeField] private Cooldown _meleeCooldown;
+
+
+
+    [Header("Range")]
+    [SerializeField] private SpawnComponent _rangeAttack;
+    [SerializeField] private Cooldown _rangeCooldown;
+
+    private Animator _animator;
+
+    private static readonly int Melee = Animator.StringToHash("melee");
+    private static readonly int Range = Animator.StringToHash("range");
+
+
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        if (_vision.isTouchingLayer)
+        {
+            if (_meleeCanAttack.isTouchingLayer)
+            {
+                if (_meleeCooldown.IsReady)
+                {
+                    MeleeAttack();
+                }
+                return;
+            }
+
+            if (_rangeCooldown.IsReady)
+            {
+                RangeAttack();
+            }
+        }
+    }
+
+    private void MeleeAttack()
+    {
+        _meleeCooldown.Reset();
+        _animator.SetTrigger(Melee);
+    }
+
+    private void RangeAttack()
+    {
+        _rangeCooldown.Reset();
+        _animator.SetTrigger(Range);
+    }
+
+    public void OnMeleeAttack()
+    {
+        _meleeAttack.Check();
+    }
+
+    public void OnRangeAttack()
+    {
+        _rangeAttack.Spawn();
+    }
+}
--- XYZ_Unity/Assets/PixelCrew/Creatures/Weapons/Projectile.cs
+++ XYZ_Unity/Assets/PixelCrew/Creatures/Weapons/Projectile.cs
@@ -7,12 +7,15 @@ namespace Pixelcrew.Creatures.Weapons
     public class Projectile : MonoBehaviour
     {
         [SerializeField] private float _speed;
+        [SerializeField] private bool _invertX;
+
         private Rigidbody2D _rigidbody;
         private int _direction;
 
         private void Start()
         {
-            _direction = transform.lossyScale.x > 0 ? 1 : -1;
+            var mod = _invertX ? -1 : 1;
+            _direction = mod * transform.lossyScale.x > 0 ? 1 : -1;
             _rigidbody = GetComponent<Rigidbody2D>();
             var force = new Vector2(_direction * _speed, 0);
             _rigidbody.AddForce(force, ForceMode2D.Impulse);
