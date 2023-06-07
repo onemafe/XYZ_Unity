2f2888ce97e7e9cfcfc8a52cd2d6b458d7fd7771
onemafe <onemafe@gmail.com>
2023-06-07 11:46:07 +0300

    Разложил компоненты по папкам

---
+++ XYZ_Unity/Assets/PixelCrew/Components/ProbabilityDropComponent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace PixelCrew.Components
+{
+	public class ProbabilityDropComponent : MonoBehaviour
+	{
+		[SerializeField] private int _count;
+		[SerializeField] private DropData[] _drop;
+		[SerializeField] private DropEvent _onDropCalculated;
+
+
+		public void CalculateDrop()
+		{
+			var itemsToDrop = new GameObject[_count];
+			var itemCount = 0;
+			var total = _drop.Sum(dropData => dropData.Probability);
+			var drop = _drop.OrderBy(dropData => dropData.Probability);
+
+
+			/* while (itemCount < _count)
+			{
+				var random = UnityEngine.Random.value * total;
+				foreach(var dropData in sortedDrop)
+				{
+					if (dropData.Probability >= random) //попали в нужное место
+					{
+						itemsToDrop[itemCount] = dropData.Drop;
+						itemCount++;
+						break;
+					}	
+				}
+			}*/
+
+			_onDropCalculated?.Invoke(itemsToDrop);
+		}
+
+
+		[Serializable]
+		public class DropData
+		{
+			public GameObject Drop;
+			[Range(0f,100f)] public float Probability;
+		}
+
+		[Serializable]
+		public class DropEvent : UnityEvent<GameObject[]>
+		{
+
+		}
+	}
+}
+
