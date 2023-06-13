3bfcefd164284ff61ced674e37611561ebff2103
onemafe <onemafe@gmail.com>
2023-06-13 12:02:22 +0300

    ShootingTrapAI переделали

--- XYZ_Unity/Assets/PixelCrew/Creatures/Mobs/ShootingTrapAI.cs
+++ XYZ_Unity/Assets/PixelCrew/Creatures/Mobs/ShootingTrapAI.cs
@@ -7,72 +7,10 @@ using UnityEngine;
 
 public class ShootingTrapAI : MonoBehaviour
 {
-    [SerializeField] private LayerCheck _vision;
-
-
-    [Header("Melee")]
-    [SerializeField] private CheckCircleOverlap _meleeAttack;
-    [SerializeField] private LayerCheck _meleeCanAttack;
-    [SerializeField] private Cooldown _meleeCooldown;
+    [SerializeField] LayerCheck _vision;
+    [SerializeField] SpriteAnimationGroup _animator;
+    [SerializeField] Cooldown _cooldown;
 
 
 
-    [Header("Range")]
-    [SerializeField] private SpawnComponent _rangeAttack;
-    [SerializeField] private Cooldown _rangeCooldown;
-
-    private Animator _animator;
-
-    private static readonly int Melee = Animator.StringToHash("melee");
-    private static readonly int Range = Animator.StringToHash("range");
-
-
-
-    private void Awake()
-    {
-        _animator = GetComponent<Animator>();
-    }
-
-
-    private void Update()
-    {
-        if (_vision.isTouchingLayer)
-        {
-            if (_meleeCanAttack.isTouchingLayer)
-            {
-                if (_meleeCooldown.IsReady)
-                {
-                    MeleeAttack();
-                }
-                return;
-            }
-
-            if (_rangeCooldown.IsReady)
-            {
-                RangeAttack();
-            }
-        }
-    }
-
-    private void MeleeAttack()
-    {
-        _meleeCooldown.Reset();
-        _animator.SetTrigger(Melee);
-    }
-
-    private void RangeAttack()
-    {
-        _rangeCooldown.Reset();
-        _animator.SetTrigger(Range);
-    }
-
-    public void OnMeleeAttack()
-    {
-        _meleeAttack.Check();
-    }
-
-    public void OnRangeAttack()
-    {
-        _rangeAttack.Spawn();
-    }
 }
