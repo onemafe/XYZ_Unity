ac29d8fdde365ff0825552e2da129ed74ef47292
onemafe <onemafe@gmail.com>
2023-06-18 15:51:00 +0300

    Добавил компонент, чуть уменьшающий громкость

---
+++ XYZ_Unity/Assets/PixelCrew/Components/Audio/ChangeVolumeDistanceComponent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class ChangeVolumeDistanceComponent : MonoBehaviour
+{
+    private Transform _goReverb;
+    private AudioReverbZone _reverbZone;
+
+    [SerializeField] private Transform _goListener;
+    [SerializeField] private AudioSource _audioSource;
+
+    [SerializeField] private float _minVolume;
+    private float _maxVolume;
+    private float distance;
+
+    private void Awake()
+    {
+        _goReverb = GetComponent<Transform>();
+        _reverbZone = GetComponent<AudioReverbZone>();
+        _maxVolume = _audioSource.volume;
+
+    }
+
+    private void Update()
+    {
+        distance = Vector3.Distance(_goReverb.position, _goListener.position);
+
+        if (distance < _reverbZone.maxDistance)
+        {
+            _audioSource.volume = Mathf.Max(_maxVolume * distance / _reverbZone.maxDistance, _minVolume);
+        }
+        
+    }
+
+
+}
