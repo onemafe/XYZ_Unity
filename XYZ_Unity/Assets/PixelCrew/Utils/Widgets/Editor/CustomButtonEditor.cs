dc6c1d28ce49bb7421bb8d9a2b5effa3b3302bb8
onemafe <onemafe@gmail.com>
2023-06-27 15:40:53 +0300

    Интерфейс ч.1

---
+++ XYZ_Unity/Assets/PixelCrew/Utils/Widgets/Editor/CustomButtonEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+[CustomEditor(typeof(CustomButton), true)]
+[CanEditMultipleObjects]
+
+
+public class CustomButtonEditor : ButtonEditor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_normal"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("_pressed"));
+        serializedObject.ApplyModifiedProperties();
+
+        base.OnInspectorGUI();
+    }
+}
